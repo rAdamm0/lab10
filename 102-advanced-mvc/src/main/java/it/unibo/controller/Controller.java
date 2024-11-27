c849e417f631893c00689dc28f40d06f53cbba99
Razzino Adam Paolo <adampaolo.razzino@studio.unibo.it>
2024-11-27 10:12:59 +0100

    Deleted useless lines of code

--- 102-advanced-mvc/src/main/java/it/unibo/controller/Controller.java
+++ 102-advanced-mvc/src/main/java/it/unibo/controller/Controller.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Controller {
     int stringNum = 0;
