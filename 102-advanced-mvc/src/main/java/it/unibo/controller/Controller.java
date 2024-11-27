f3b4810c0bcc3dbe1643d4605af8360d65327ee2
Razzino Adam Paolo <adampaolo.razzino@studio.unibo.it>
2024-11-27 10:03:33 +0100

    Wrote a first version of the readLine methods in controller

--- 102-advanced-mvc/src/main/java/it/unibo/controller/Controller.java
+++ 102-advanced-mvc/src/main/java/it/unibo/controller/Controller.java
@@ -1,5 +1,36 @@
 package it.unibo.controller;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Controller {
+    int stringNum = 0;
+
+    public final void setLine(int n){
+        this.stringNum=n;
+    }
+
+    public final String readLine(String input, int n){
+        try (BufferedReader toRead = new BufferedReader(new FileReader(input))) {
+            String result;
+            int i = 0;
+            do{
+                result = toRead.readLine();
+                i++;
+            }while(i<n);
+            return result;
+        } catch (IOException e) {
+            e.getStackTrace();
+            return null;
+        }
+    }
+
+
+    public final String readLine(File input, int n){
+
+        return null;
+    }
 
 }
